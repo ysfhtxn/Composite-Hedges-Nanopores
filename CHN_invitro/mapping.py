709c4a1a43c13a32a71db61da6fb7fabe2296a47
12132451 <12132451@mail.sustech.edu.cn>
2024-06-24 19:55:46 +0800

    mapping.py & readsdic_gen.py update

--- CHN_invitro/mapping.py
+++ CHN_invitro/mapping.py
@@ -1,13 +1,14 @@
 import os
+from datetime import datetime
 
 # reference .fasta file
 fapath = r'/home/lijy/workspace/fasta/barcodeljy_384.fasta'
 
 # input basecalled .fastq path
-inputpath = r'/data/biolab-nvme-pool1/fanqy/tmp/bonito-train/fastq/'
+inputpath = r'/data/biolab-nvme-pool1/lijy/guppy_basecalled/va_qscore/smeseqs_ex_231120/smeseqs_ex_q16/'
 
 # output .paf files path
-outputpath = r'./fqy_output/'
+outputpath = r'/data/biolab-nvme-pool1/lijy/mapping_res/va_qscore/paf_smeseqs_ex_231120_barcode_custom/smeseqs_ex_q16/'
 
 filelist = os.listdir(inputpath)
 os.makedirs(outputpath, exist_ok=True)
@@ -19,38 +20,56 @@ file_raw_list = list(
     filter(lambda filename: filename[-len('.fastq'):] == '.fastq', filelist))
 
 # custom parameters
-k_mer_size = 12 #12
-minimizer_window_size = 8 #8
-minimal_chaining_score = 15 #15
-n_discard_chains_minimizers_le = 2
-minimal_peak_DP_alignment_score_to_output  = 15
+CUSTOM = True
 
-# config file to save custom parameters info
-f = open(os.path.join(outputpath,'config.txt'), 'w')
-f.write('k_mer_size: %d\n' % k_mer_size)
-f.write('minimizer_window_size: %d\n' % minimizer_window_size)
-f.write('minimal_chaining_score: %d\n' % minimal_chaining_score)
-f.write('n_discard_chains_minimizers_le: %d\n' % n_discard_chains_minimizers_le)
-f.write('minimal_peak_DP_alignment_score_to_output: %d\n' % minimal_peak_DP_alignment_score_to_output)
-f.close()
+k_mer_size = 12 # -k[15]
+minimizer_window_size = 8 # -w[10]
+minimal_chaining_score = 15 # -m[40]
+n_discard_chains_minimizers_le = 2 # -n [3]
+minimal_peak_DP_alignment_score_to_output  = 15 # -s [40]
 
 # CPU occupation
-threads = 8
+threads = 12
 
+# config file to save custom parameters info
+if CUSTOM:
+    f = open(os.path.join(outputpath,'config.txt'), 'w')
+    f.write('k_mer_size: %d\n' % k_mer_size)
+    f.write('minimizer_window_size: %d\n' % minimizer_window_size)
+    f.write('minimal_chaining_score: %d\n' % minimal_chaining_score)
+    f.write('n_discard_chains_minimizers_le: %d\n' % n_discard_chains_minimizers_le)
+    f.write('minimal_peak_DP_alignment_score_to_output: %d\n' % minimal_peak_DP_alignment_score_to_output)
+    f.close()
+
+
+print('Start minimap2 mapping... ', flush=True)
+print('fasta file: ', fapath)
+print('inputpath: ', inputpath)
+print('outputpath: ', outputpath)
+print('Minimap2 version: ', minimap2_path, flush=True)
+print('mapping params: CUSTOM? ', CUSTOM, flush=True)
+
+start_time = datetime.now()
+print('start time: ', start_time, flush=True)
+print('\n\n')
 
 for i in range(len(file_raw_list)):
     curfile = file_raw_list[i]
-    # default preset params
-    # command = '%s -cx map-ont %s %s%s > %s%s.paf' % (minimap2_path, fapath,inputpath,curfile,outputpath,curfile)
-    
-    # default custom params
-    command = f"{minimap2_path} -c -k{k_mer_size} -w{minimizer_window_size} -n{n_discard_chains_minimizers_le} -m{minimal_chaining_score} " \
-              f"-s{minimal_peak_DP_alignment_score_to_output} -t{threads} {fapath} {inputpath+curfile} > {outputpath+curfile}.paf"
-
+    if CUSTOM :
+        command = f"{minimap2_path} -c -k{k_mer_size} -w{minimizer_window_size} -n{n_discard_chains_minimizers_le} -m{minimal_chaining_score} " \
+                f"-s{minimal_peak_DP_alignment_score_to_output} -t{threads} {fapath} {inputpath+curfile} > {outputpath+curfile}.paf"
+    else:
+        command = f'{minimap2_path} -cx map-ont {fapath} {inputpath+curfile} > {outputpath+curfile}.paf'
     print('processing %s' % curfile)
     os.system(command)
 
-	# break for check
+	# # break for check
     # break
+
+
+end_time = datetime.now()
+duration = end_time - start_time
+print('end time: ', end_time, flush=True)
+print('duration time: ', duration, flush=True)
 
 
--- CHN_invitro/readsdic_gen.py
+++ CHN_invitro/readsdic_gen.py
@@ -3,46 +3,49 @@ import pandas as pd
 import pickle
 import tqdm
 
+# PAF file header
+columns = ['Query_sequence_name', 'Query_sequence_length', 'Query_start', 'Query_end', 'Relative_strand',
+        'Target_sequence_name', 'Target_sequence_length', 'Target_start_on_original_strand',
+        'Target_end_on_original_strand', 'Number_of_residue_matches', 'Alignment_block_length', 'Mapping_quality',
+        'NM', 'ms', 'AS', 'nn', 'tp', 'cm','s1','s2','de','rl','cg']
+
 # input paf files path
-input_paf_paths = [
-    r'/home/lijy/workspace/dna_storage/sequencing_qc/fqy_output/'
-    ]
+input_paf_paths = r'/data/biolab-nvme-pool1/lijy/mapping_res/va_qscore/paf_smeseqs_230703_2_barcode_custom/smeseqs_230703_2_q16/'
 
 # output dictionary file
-output_file = r'./fqy_smeseqs_q15_barcode_k12w8m15n2s15_mq5.P'
+output_file = r'/data/biolab-nvme-pool1/lijy/smeseqs/smeseqs_230703_2_q16.P'
 
 reads_hash_dic = {}
 
 # depend on minimap2 results to filter reads
-mapping_qulity_threshold = 5
+mapping_qulity_threshold = 3
 
-for inputpath in input_paf_paths:
-    print('processing %s' % inputpath)
-    filelist = os.listdir(inputpath)
-    file_raw_list = list(
-        filter(lambda filename: filename[-4:] == '.paf', filelist))
-    file_raw_list = list(
-        filter(lambda filename: filename[-4:] == '.paf', filelist))
-    with tqdm.trange(len(file_raw_list), desc='reading pafs') as tbar:
+filelist = os.listdir(input_paf_paths)
+file_raw_list = list(filter(lambda filename: filename[-len('.paf'):] == '.paf', filelist))[:]
 
-        for i in range(len(file_raw_list)):
-            curfile = file_raw_list[i]
-            data = pd.read_csv(os.path.join(inputpath, curfile), sep='\t', on_bad_lines='skip')
-            mapping_q = data['Mapping_quality'].to_list()
-            query_s = data['Query_start'].to_list()
-            query_e = data['Query_end'].to_list()
-            query_len = data['Query_sequence_length'].to_list()
-            query_name = data['Query_sequence_name'].to_list()
-            target_name = data['Target_sequence_name'].to_list()
-            relative_strand = data['Relative_strand'].to_list()
 
-            for j in range(len(mapping_q)):
-                if mapping_q[j] >= mapping_qulity_threshold:
+with tqdm.tqdm(total=len(file_raw_list), desc=f"processing...") as pbar:
+    for curfile in file_raw_list:
+        data = pd.read_csv(os.path.join(input_paf_paths, curfile), sep='\t', on_bad_lines='skip')
+        mapping_q = data['Mapping_quality'].to_list()
+        query_s = data['Query_start'].to_list()
+        query_e = data['Query_end'].to_list()
+        query_len = data['Query_sequence_length'].to_list()
+        query_name = data['Query_sequence_name'].to_list()
+        target_name = data['Target_sequence_name'].to_list()
+        relative_strand = data['Relative_strand'].to_list()
+
+        
+        for j in range(len(mapping_q)):
+            if mapping_q[j] >= mapping_qulity_threshold:
+                if query_name[j] not in reads_hash_dic:
                     reads_hash_dic[query_name[j]] = {
-                        'Q_s':query_s[j], 'Q_e':query_e[j], 'Q_l':query_len[j],
-                        'T_n':target_name[j], 'M_q':mapping_q[j], 'R_s':relative_strand[j]
+                    'Q_s':query_s[j], 'Q_e':query_e[j], 'Q_l':query_len[j],
+                    'T_n':target_name[j], 'M_q':mapping_q[j], 'R_s':relative_strand[j]
                     }
-            tbar.update()
+                elif query_name[j] in reads_hash_dic and reads_hash_dic[query_name[j]]['M_q'] < mapping_q[j]:
+                    reads_hash_dic[query_name[j]]['M_q'] = mapping_q[j]
+        pbar.update()
 
 
 print('writing to file: %s' % output_file)
